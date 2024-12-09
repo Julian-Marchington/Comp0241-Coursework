29933e538006d55231ce4ff0d19200635c14445d
Julian <ucabj62@ucl.ac.uk>
2024-12-09 17:05:32 +0000

    Finished Colour Detection, added ColourRangeDetector.py

--- Task1_ColourDetection.py
+++ Task1_ColourDetection.py
@@ -9,39 +9,32 @@ from PIL import Image
 from collections import deque
 
 # Attempting to retrieve binary mask from image
-image = cv2.imread('/Users/julianmarchington/Desktop/Comp0241-Coursework/Dataset/images/000046.png')
+image = cv2.imread('/Users/julianmarchington/Desktop/Comp0241-Coursework/Dataset/images/000030.png')
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Define blue range
-lower_blue = np.array([100, 50, 50])  # Adjust for your image
+lower_blue = np.array([100, 50, 50]) 
 upper_blue = np.array([140, 255, 255])
 
-# Define white range
-lower_white = np.array([90, 10, 180])  # Light blue/white-ish range
-upper_white = np.array([130, 60, 255])
+lower_clouds = np.array([85, 5, 180]) 
+upper_clouds = np.array([135, 80, 255])
 
-lower_clouds = np.array([85, 5, 180])  # Lower Saturation for faint clouds
-upper_clouds = np.array([135, 80, 255])  # Higher Value to capture bright clouds
+lower_test = np.array([60, 0, 0])
+upper_test = np.array([179, 173, 134])
 
-# Green range for vegetation
-lower_green = np.array([30, 40, 40])  # Greenish tones
-upper_green = np.array([90, 255, 255])
-
-# Brown/tan range for land
-lower_brown = np.array([10, 40, 40])  # Earthy tones
-upper_brown = np.array([30, 255, 200])  # Adjust upper limit if needed
+lower_bound = np.array([69, 20, 0])
+upper_bound = np.array([179, 255, 255])
 
 # Create masks
 blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
 white_mask = cv2.inRange(hsv, lower_clouds, upper_clouds)
-#green_mask = cv2.inRange(hsv, lower_green, upper_green)
-#brown_mask = cv2.inRange(hsv, lower_brown, upper_brown)
+test_mask = cv2.inRange(hsv, lower_test, upper_test)
+another_mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
 # Combine the masks
 combined_mask = cv2.bitwise_or(blue_mask, white_mask)
-#combined_mask = cv2.bitwise_or(combined_mask, green_mask)
-#combined_mask = cv2.bitwise_or(combined_mask, brown_mask)
+combined_mask = cv2.bitwise_or(combined_mask, test_mask)
+combined_mask = cv2.bitwise_or(combined_mask, another_mask)
 
 # Apply morphological operations
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
